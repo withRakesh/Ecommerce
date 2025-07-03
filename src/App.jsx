d69b969b6a37a01bf25ef0cf08710387bbd59fd7
withRakesh <rakesh2003cs@gmail.com>
2025-07-03 13:58:54 +0530

    react lazy updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React, { useState , Suspense} from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MyNavbar from "./components/Navbar/Navbar";
-import Navigation from "./components/ProductsNavigation/Navigation";
-import Home from "./pages/Home";
-import Laptops from "./pages/Laptops";
-import Watches from "./pages/Watches";
-import Headphones from "./pages/Headphones";
-import Footer from "./components/footer/Footer";
-import CartPage from "./pages/CartPage";
 import {ToastContainer, toast} from "react-toastify"
+const MyNavbar = React.lazy(() => import('./components/Navbar/Navbar')); 
+ const Navigation = React.lazy(() => import('./components/ProductsNavigation/Navigation'))
+const Home = React.lazy(()=> import('./pages/Home'))  
+const Laptops = React.lazy(()=> import('./pages/Laptops')); 
+const Watches = React.lazy(()=>import('./pages/Watches')); 
+const Headphones = React.lazy(()=>import('./pages/Headphones')); 
+const Footer = React.lazy(()=>import('./components/footer/Footer')) 
+const CartPage = React.lazy(()=>import('./pages/CartPage'))
  
 const App = () => {
   const [cartItems, setCartItems] = useState([]);
@@ -24,6 +24,7 @@ const App = () => {
 
   return (
     <BrowserRouter>
+    <Suspense fallback={<p>Loading page...</p>}>
     <ToastContainer position="top-center" autoClose={1000}/> 
       <MyNavbar cartItems={cartItems}  />
       <Navigation />
@@ -37,6 +38,8 @@ const App = () => {
       </Routes>
 
       <Footer />
+    </Suspense>
+ 
     </BrowserRouter>
   );
 };
